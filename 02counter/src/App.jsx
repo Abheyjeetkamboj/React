c843265ad197c5df3a6c37dae7858a8a5ab82353
Abheyjeet Kamboj <Abheyjeet100@gmail.com>
2026-08-05 06:39:07 +0530

    hooks and project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,11 +5,27 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+let[counter,setcounter] = useState(5);
+const addvalue = ()=>{
+  counter=counter+1;
+  setcounter(counter)
+  console.log("clicked "+counter)
+}
+const removed = ()=>{
+  counter=counter-1;
+  setcounter(counter)
+  console.log("clicked "+counter)
+}
   return (
     <>
-    <h1></h1>
+    <h1>Chai Aur Code</h1>
+    <h2>Counter Value : {counter}</h2>
+    
+    
+    <button onClick={addvalue}>Add Value {counter}</button>
+    <br/>
+    <button onClick={removed}>Remove Value {counter}</button>
+    <footer> footer: {counter}</footer>
     </>
   )
 }
